051e7760f64d1801fbbc2634a083d15a19f22dfd
Ayodeji Ayibiowu <ayodeji.ayibiowu@getinge.com>
2025-05-11 01:11:42 +0200

    chore: cleanup crew

--- mergebot/crews/mr_processor/crew.py
+++ mergebot/crews/mr_processor/crew.py
@@ -10,26 +10,12 @@ class MRProcessor(BotBaseCrew):
     """MRProcessor crew"""
 
     @agent
-    def mr_input_handler(self) -> Agent:
-        return Agent(
-            config=self.agents_config["mr_input_handler"],
-            llm=self.llm_model,
-            verbose=True,
-        )
-
-    @agent
     def mr_retriever(self) -> Agent:
         return Agent(
             config=self.agents_config["mr_retriever"],
             llm=self.llm_model,
             tools=[GitlabMergeRequestTool(result_as_answer=True)],
             verbose=False,
-        )
-
-    @task
-    def mr_input_task(self) -> Task:
-        return Task(
-            config=self.tasks_config["mr_input_task"],
         )
 
     @task
