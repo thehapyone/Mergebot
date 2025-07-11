d345152c8b67c853ae532c36fb5abb0c7eaefbcb
Ayodeji Ayibiowu <ayodeji.ayibiowu@getinge.com>
2025-07-11 10:43:07 +0200

    Refactor imports in crew.py files to remove unused modules

--- mergebot/crews/code_analysis/crew.py
+++ mergebot/crews/code_analysis/crew.py
@@ -1,5 +1,5 @@
-from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
+from crewai import Agent, Task
+from crewai.project import CrewBase, agent, task
 
 from mergebot.crews.commons import BotBaseCrew
 
